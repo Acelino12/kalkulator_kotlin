2d76328df09342d1117435e9dc19fbc4cc93b4cd
marselino kartawijaya <90839452+Acelino12@users.noreply.github.com>
2023-12-16 14:02:37 +0700

    pembetulan viewModel

--- app/src/main/java/com/example/challenge_rekamin_satu/MainActivity.kt
+++ app/src/main/java/com/example/challenge_rekamin_satu/MainActivity.kt
@@ -3,19 +3,12 @@ package com.example.challenge_rekamin_satu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.challenge_rekamin_satu.ui.theme.Challenge_rekamin_satuTheme
 import com.example.challenge_rekamin_satu.ui.theme.MediumGray
 
@@ -24,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Challenge_rekamin_satuTheme {
-                val viewModel = viewModels<ViewModelKalkulator>()
+                val viewModel = viewModel<ViewModelKalkulator>()
                 val state = viewModel.state
                 val buttonSpacing = 8.dp
                 Kalkulator(
